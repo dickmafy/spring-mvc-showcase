fe895094881da8b2837f35c258002a486e8f49cb
dickmafy <diego.matos.b@gmail.com>
2015-03-18 12:50:51 -0500

    Update CallableController.java

--- spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/async/CallableController.java
+++ spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/async/CallableController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.async.WebAsyncTask;
-//PCDIEGO2
+//chrome
 @Controller
 @RequestMapping("/async/callable")
 public class CallableController {
